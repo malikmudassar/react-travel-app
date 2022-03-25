068beceeb130aacdc4852d98e186f275ce66d918
omar najmi <onajmi@asu.edu>
2022-03-25 10:38:15 +0500

    readme

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect, createRef } from 'react';
+import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from '@material-ui/core';
+
+// import PlaceDetails from '../PlaceDetails/PlaceDetails';
+import useStyles from './styles.js';
 
 const List = () => {
-    return (
-        <h1>List</h1>
+  const [elRefs, setElRefs] = useState([]);
+  const classes = useStyles(); 
+  const [type, setType] = useState('restaurants');
+  const [rating, setRating] = useState('0');
+  
+  return (
+      
+      <div className={classes.container}>
+      <Typography variant="h4">Food & Dining around you</Typography>
+      
+        <>
+          <FormControl className={classes.formControl}>
+            <InputLabel id="type">Type</InputLabel>
+            <Select id="type" value={type} onChange={(e) => setType(e.target.value)}>
+              <MenuItem value="restaurants">Restaurants</MenuItem>
+              <MenuItem value="hotels">Hotels</MenuItem>
+              <MenuItem value="attractions">Attractions</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl className={classes.formControl}>
+            <InputLabel id="rating">Rating</InputLabel>
+            <Select id="rating" value={rating} onChange={(e) => setType(e.target.value)}>
+              <MenuItem value="0">All</MenuItem>
+              <MenuItem value="3">Above 3.0</MenuItem>
+              <MenuItem value="4">Above 4.0</MenuItem>
+              <MenuItem value="4.5">Above 4.5</MenuItem>
+            </Select>
+          </FormControl>
+          
+        </>
+      
+    </div>
     );
 }
 
